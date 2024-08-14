609409f5a71b566879ad5a53eae47c6b2658b302
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-08-14 21:36:23 +0530

    Updated configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/',
+  base: '/Ochi-design_clone_using_react_and_tailwind/',
 })
